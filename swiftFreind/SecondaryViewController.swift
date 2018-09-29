cae6dc9911ad6fa371b01bd3458a023a137be63b
weerawat poseeya <weerawat.p@digitalbutler.co.th>
2018-09-29 11:58:03 +0700

    item button navigator

---
+++ swiftFreind/SecondaryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondaryViewController.swift
+//  swiftFreind
+//
+//  Created by weerawat poseeya on 29/9/2561 BE.
+//  Copyright © 2561 butler. All rights reserved.
+//
+
+import UIKit
+
+class SecondaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
