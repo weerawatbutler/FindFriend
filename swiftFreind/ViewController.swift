ffa15a53a74a4c58772644921627133b0dc44a9b
weerawat poseeya <weerawat.p@digitalbutler.co.th>
2018-09-29 11:39:04 +0700

    table view

--- swiftFreind/ViewController.swift
+++ swiftFreind/ViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let friendStrings = ["pream", "new", "sea", "kay", "ming", "cla"]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friendStrings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = friendStrings[indexPath.row]
+        return cell
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
